650a9822605dc21626c25c0947f819be883243f7
Talley Lambert <talley.lambert@gmail.com>
2023-12-30 16:32:32 -0500

    updating biotium import

--- fpbase_scripts/biotium.py
+++ fpbase_scripts/biotium.py
@@ -1,21 +1,17 @@
-from fileinput import isfirstline
 from pathlib import Path
 from django.utils.text import slugify
 import pandas as pd
 import math
 from proteins.models import Dye
-from proteins.models import Spectrum
 from django.db import transaction
+import numpy as np
 
 DATA_DIR = Path(__file__).parent.parent / "data"
 BIOTIUM_DYES = DATA_DIR / "Biotium-121823-dye-list.csv"
 BIOTIUM_DATA = DATA_DIR / "Biotium-121823-data.csv"
 
 
-existing = {
-    n: s
-    for n, s in Dye.objects.filter(manufacturer="Biotium").values_list("name", "slug")
-}
+existing = {d.name: d for d in Dye.objects.filter(manufacturer="Biotium")}
 
 # names that need to be changed to match the names in the data file
 # map of new name to old name
@@ -32,6 +28,7 @@ NAME_CHANGES = {
     "BactoView Live Green": "BactoView Green",
     "PE (R-PE / R-phycoerythrin)": "PE (R-PE, R-phycoerythrin)",
 }
+SKIP_NAMES = {"RFP", "YFP", "mCherry", "CFP", "EGFP", "EGFP (GFP)", "Hoechst"}
 
 # these are the names of dyes that have been attributed to Biotium
 # but which are not in the Biotium data file
@@ -46,33 +43,61 @@ def add_biotium_data():
 
     dyes = pd.read_csv(BIOTIUM_DYES)
     data = pd.read_csv(BIOTIUM_DATA)
-
+    processed = set()
     # note all the dye names have a space at the end
-    _waves = list(data["Wavelength(nm)"])
+
     for _, (name, manufacturer, *__) in dyes.iterrows():
         name = name.strip()
         if isinstance(manufacturer, float) and math.isnan(manufacturer):
             manufacturer = ""
+        if name in SKIP_NAMES:
+            print(f"❌ Skipping {name}")
+            continue
+        processed.add(name)
         slug = slugify(name.replace("/", "-"))
 
         # get the dye instance
-        if name in NAME_CHANGES:
+        if (
+            name in NAME_CHANGES
+            and Dye.objects.filter(name=NAME_CHANGES[name]).exists()
+        ):
             # check for existing dyes that need name changes
             dye = Dye.objects.get(name=NAME_CHANGES[name])
             dye.name = name
             dye.slug = slug
+            print(f"🆙 Updated {NAME_CHANGES[name]}->{name}")
         else:
             # otherwise look for a dye with the same slug
             try:
                 dye = Dye.objects.get(slug=slug)
+                print(f"👀 Found {name}")
             except Dye.DoesNotExist:
+                if manufacturer != "Biotium":
+                    print(f"❌ Skipping {name}")
+                    continue
                 dye = Dye(name=name, slug=slug)
-                # print(f"Created {name}")
+                print(f"✨ Created {name} ({manufacturer})")
 
         # update manufacturer ... in case it was misattributed to Biotium
         dye.manufacturer = manufacturer
         if not manufacturer:
             dye.url = ""
+        dye.save()
 
-        _data_abs = data[name + "Abs"]
-        _data_em = data[name + "Em"]
+        abs_data = np.asarray(data[["Wavelength(nm)", name + " Abs"]].dropna()).tolist()
+        dye.spectra.update_or_create(
+            owner_dye=dye, subtype="ab", category="d", defaults={"data": abs_data}
+        )
+
+        em_data = np.asarray(data[["Wavelength(nm)", name + " Em"]].dropna()).tolist()
+        dye.spectra.update_or_create(
+            owner_dye=dye, subtype="em", category="d", defaults={"data": em_data}
+        )
+
+    # check existing dyes that aren't in the data file
+    for name, dye in existing.items():
+        if name not in processed.union(NAME_CHANGES.values()):
+            print(f"🔗 Disowning {dye}")
+            dye.manufacturer = ""
+            dye.url = ""
+            # dye.save()
